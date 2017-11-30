69a59ef6e8cf9cddab4a8f5793c35c543b1257ab
Victor Campusano <vcampusano@outlook.com>
2017-11-29 22:56:21 -0400

    Prueba de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase,FirebaseListObservable } from 'angularfire2/database';
+//import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,149 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hola Mundo';
+  nombre = '';
+ /* misnotas = [
+    {
+      id: 1, titulo: 'Nota', descripcion: 'Una descripcion for nota 1'
+    },
+    {
+      id: 2, titulo: 'Nota', descripcion: 'Una descripcion for nota 2'
+    },
+    {
+      id: 3, titulo: 'Nota', descripcion: 'Una descripcion for nota 3'
+    },
+    {
+      id: 4, titulo: 'Nota', descripcion: 'Una descripcion for nota 4'
+    },
+
+
+  ];*/
+
+  nota = { id: null, titulo: null, descripcion: null };
+
+  mostrar_formulario = false;
+  editing = false;
+
+  misnotas: any;
+
+  constructor(public afDB: AngularFireDatabase) {
+   // this.misnotas = afDB.list('notas').valueChanges();
+    if(navigator.onLine){
+      this.mostrarNotas().valueChanges().subscribe(notas => { 
+          this.misnotas = notas ; 
+        //guardar localmente
+          localStorage.setItem('misnotas',JSON.stringify(this.misnotas));
+        }
+      );
+
+    }
+    else{
+      this.misnotas = JSON.parse(localStorage.getItem('misnotas'));
+    }
+    
+  }
+
+  agregarNota() {
+    this.mostrar_formulario = true;
+
+  }
+
+  mostrarNotas(){
+    return this.afDB.list('/notas');
+  }
+
+  verNota(lanota) {
+    this.editing = true;
+
+    this.nota = lanota;
+    this.mostrar_formulario = true;
+
+  }
+  cancelar() {
+    this.mostrar_formulario = false;
+  }
+
+  eliminarNota(){
+    if(navigator.onLine){
+      this.afDB.database.ref('notas/' + this.nota.id).remove();
+    }
+    else{
+      this.misnotas.forEach((nota,i)=>{ if(nota.id == this.nota.id){ this.misnotas.splice(i,1) }});
+
+    }
+    
+    this.nota = { id: null, titulo: null, descripcion: null };
+    localStorage.setItem('misnotas',JSON.stringify(this.misnotas));
+    this.mostrar_formulario = false;
+
+
+  }
+
+ /* eliminarNota() {
+    this.mostrar_formulario = false;
+    var me = this; //porque forearch crea su propio scope
+    this.misnotas.forEach(function (e, i) {
+      if (e.id == me.nota.id) {
+        me.misnotas.splice(i, 1);
+        //me.misnotas[i] = me.nota; //le asigna el elemento que esta siendo editado
+      }
+    });
+    this.nota = { id: null, titulo: null, descripcion: null };
+    this.mostrar_formulario = false;
+
+
+  }
+  */
+
+  crearNota() {
+    /*if (this.editing) {
+      // alert('Estamos editando');
+      var me = this; //porque forearch crea su propio scope
+      this.misnotas.forEach(function (e, i) {
+        if (e.id == me.nota.id) {
+          me.misnotas[i] = me.nota; //le asigna el elemento que esta siendo editado
+        }
+      });
+      this.mostrar_formulario = false;
+      this.editing = false;
+    }
+    else {
+      this.nota.id = Date.now();
+      this.misnotas.push(this.nota);
+      this.mostrar_formulario = false;
+      this.nota = { id: null, titulo: null, descripcion: null };
+
+    }
+    */
+   // this.nota.id = Date.now();
+    if (this.editing) {
+      if(navigator.onLine){
+        this.afDB.database.ref('notas/' + this.nota.id).set(this.nota);
+      }  
+      else{
+        
+        this.misnotas.forEach((nota)=>{ if(nota.id == this.nota.id){ nota = this.nota; }});
+      }
+      
+      //this.editing = false;
+    } else { 
+     
+      this.nota.id = Date.now();   
+      if(navigator.onLine){
+        this.afDB.database.ref('notas/' + this.nota.id).set(this.nota);
+      }  
+      else{
+        this.misnotas.push(this.nota);
+      }
+  }
+
+  
+  this.mostrar_formulario = false;
+  this.nota = { id: null, titulo: null, descripcion: null };
+  localStorage.setItem('misnotas',JSON.stringify(this.misnotas));
+
+}
+ 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBmZHTZcoQpufgvbGuMVRMY8sAyqBq7kuU",
+  authDomain: "ng-notastuto.firebaseapp.com",
+  databaseURL: "https://ng-notastuto.firebaseio.com",
+  storageBucket: "ng-notastuto.appspot.com",
+  messagingSenderId: "1033096012674"
+};
 
 
 @NgModule({
@@ -11,6 +24,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    ,FormsModule
+    ,[AngularFireModule.initializeApp(firebaseConfig)]
+    ,AngularFireDatabaseModule
+    ,AngularFireAuthModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
